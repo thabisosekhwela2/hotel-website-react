016c113ed1aedfeff88491f4eccf48f762a0ffd4
thabisosekhwela2 <thabisosekhwela46@gmail.com>
2023-11-27 12:33:41 +0200

    component breakdown

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Test
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+
+        <Header/>
+
+        <Main/>
+
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
